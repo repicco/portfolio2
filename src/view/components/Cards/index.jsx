e4537dd2ea5d58c5b22b63f762994e02ed682c39
renato.picco.glo <renato.picco@globalhitss.com.br>
2025-07-01 22:48:04 -0300

    feat: create repo links for projects

--- src/view/components/Cards/index.jsx
+++ src/view/components/Cards/index.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import "./cards.css";
 
-export default function Cards({ img, title, text, link }) {
-  function handleNavigation() {
+export default function Cards({ img, title, text, link, repo }) {
+  function clickCardNavigation() {
     window.open(link, "_blank");
   }
 
+  function clickRepo(){
+    window.open(repo, "_blank");
+  }
+
   return (
-    <div className="card" onClick={handleNavigation}>
-      <img src={img} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{text}</p>
+    <>
+    <div className="card" >
+      <div className='card-content' onClick={clickCardNavigation}>
+        <img src={img} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{text}</p>
+        </div>
       </div>
+      <hr/>
+      <div className='card-gitlink' onClick={clickRepo}>
+          <i class="fa-brands fa-github " />
+          Repositório
+        </div>
     </div>
+     
+    </>
+    
   );
 }
--- src/view/pages/Projects/index.jsx
+++ src/view/pages/Projects/index.jsx
@@ -19,54 +19,63 @@ const cards = [
     title: "Restaurante Online",
     text: "NextJs + PostgreeDB + TailwindCss + RestApi",
     link: "https://nextjs-donalds.vercel.app/fsw-donalds",
+    repo: "https://github.com/repicco/nextjs-donalds",
   },
   {
     img: filmes,
     title: "Filmes",
-    text: "Vue Js + Flex + RestApi",
-    link: "https://filmes-vuejs.netlify.app/",
+    text: "React Js + Flex + RestApi",
+    link: "https://filmespicco.netlify.app/",
+    repo: "https://github.com/repicco/24.FabricaApps/tree/master/Projetos/filme",
   },
   {
     img: blogdev,
     title: "Blog Programador",
     text: "Firebase + React Js + Flexbox",
     link: "https://blogdev.netlify.app/",
+    repo: "https://github.com/repicco/24.FabricaApps/tree/master/Projetos/blog",
   },
   {
     img: pet,
     title: "PetStore",
     text: "Flexbox + React Js + ReactSlick",
     link: "https://seri-e.netlify.app/",
+    repo: "https://github.com/repicco/TestSeri.e",
   },
   {
     img: ingles,
     title: "Ingles",
     text: "Materialize + React Js + ReactSlick",
     link: "https://mariadrozina.netlify.app/",
+    repo: "https://github.com/repicco/28.Ingles.Maria",
   },
   {
     img: cronometro,
     title: "Cronometro",
     text: "Flexbox + React Js",
     link: "https://cronometropicco.netlify.app/",
+    repo: "https://github.com/repicco/24.FabricaApps/tree/master/Projetos/cronometro",
   },
   {
     img: finance,
     title: "Financiamento",
     text: "Materialize + React Js",
     link: "https://originfinance.netlify.app/",
+    repo: "https://github.com/repicco/27.OriginFinance",
   },
   {
     img: churras,
     title: "Churrasco",
     text: "Bootstrap + Javascript (Vanilla)",
     link: "https://churras.netlify.app/",
+    repo: "https://github.com/repicco/23.Bootstrap-JS.ChurrascoCalc",
   },
   {
     img: spotify,
     title: "Spotify Layout",
     text: "Bootstrap",
     link: "https://spotify-fake.netlify.app/",
+    repo: "https://github.com/repicco/08.Bootstrap.SpotifyFake",
   },
 ];
 
@@ -74,7 +83,7 @@ export default function History() {
   return (
     <div className="container">
       {cards.map((el) => (
-        <Cards img={el.img} title={el.title} text={el.text} link={el.link} />
+        <Cards img={el.img} title={el.title} text={el.text} link={el.link} repo={el.repo} />
       ))}
     </div>
   );
