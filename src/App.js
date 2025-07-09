eb137a2eb69fb4100ba9345c00d5b0e28b06b805
renato.picco.glo <renato.picco@globalhitss.com.br>
2025-07-09 13:01:33 -0300

    feat: update apps carroussel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./modal/router";
 
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 function App() {
   return <RouterProvider router={router} />;
 }
--- src/view/pages/ProjectsApps/index.jsx
+++ src/view/pages/ProjectsApps/index.jsx
@@ -1,30 +1,97 @@
 import Lottie from "react-lottie";
 import { useResponsive } from "../../../hooks/useResponsive";
 import calcBarbecueLottie from "../../assets/lottie_files/calc_barbecue_app.lottie.json";
+import financialLottie from "../../assets/lottie_files/financial_app.lottie.json";
+import shoppingCartLottie from "../../assets/lottie_files/shopping_cart_app.lottie.json";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "./projectsApps.css";
+
+import { Navigation, Pagination } from "swiper/modules";
 
 export default function ProjectsApps() {
   const { windowSize } = useResponsive();
   const { width } = windowSize;
   const isTabletScreen = width > 768;
 
+  function clickRepo(link) {
+    window.open(link, "_blank");
+  }
+
+  if (!width) return null;
+
+  const carouselData = [
+    {
+      title: "Cálculo de churrasco",
+      tech: "React Native",
+      lottieFile: calcBarbecueLottie,
+      linkRepo: "https://github.com/repicco/calc-barbecue",
+      alt: "Animação do app: cálculo de churrasco",
+    },
+    {
+      title: "Finanças",
+      tech: "React Native",
+      lottieFile: financialLottie,
+      linkRepo: "https://github.com/repicco/app_financial",
+      alt: "Animação do app: finanças",
+    },
+    {
+      title: "Carrinho de compras",
+      tech: "React Native",
+      lottieFile: shoppingCartLottie,
+      linkRepo: "https://github.com/repicco/shopping_cart",
+      alt: "Animação do app: carrinho de compras",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Projetos Apps</h1>
-      <Lottie
-        options={{
-          loop: true,
-          autoplay: true,
-          animationData: calcBarbecueLottie,
-          rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice",
-          },
-          margin: 0,
-        }}
-        width={isTabletScreen ? "228px" : "128px"}
-        height={isTabletScreen ? "500px" : "272px"}
-        style={{ borderRadius: isTabletScreen ? "28px" : "20px" }}
-        alt="Tutorial animado"
-      />
+    <div className="projectApps__container">
+      <div className="projectApps__swipper">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={30}
+          slidesPerView={isTabletScreen ? 2 : 1.5}
+          loop={true}
+          pagination={{ clickable: true }}
+        >
+          {carouselData.map((el) => (
+            <SwiperSlide>
+              <div className="projectApps__card">
+                <Lottie
+                  options={{
+                    loop: true,
+                    autoplay: true,
+                    animationData: el.lottieFile,
+                    rendererSettings: {
+                      preserveAspectRatio: "xMidYMid slice",
+                    },
+                  }}
+                  width={isTabletScreen ? "228px" : "128px"}
+                  height={isTabletScreen ? "500px" : "272px"}
+                  style={{
+                    borderRadius: isTabletScreen ? "28px" : "20px",
+                    display: "block",
+                    margin: "0 16px",
+                  }}
+                  alt={el.alt}
+                />
+                <div>
+                  <h4>{el.title}</h4>
+                  <p>{el.tech}</p>
+                  <div
+                    className="projectApps__repoButton"
+                    onClick={() => clickRepo(el.linkRepo)}
+                  >
+                    <i class="fa-brands fa-github " />
+                    Repositório
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 }
